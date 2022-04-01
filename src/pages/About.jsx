aef71418c8dae9d1c542e27a6a458c57a1390794
Tima Baisalov <baisalov.t98@gmail.com>
2022-04-01 11:08:32 +0600

    react

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <h1>
-            Это приложение создано в качестве обуч. ролика на ULBI TV
+          Повторил все темы в react
         </h1>
     );
 };
